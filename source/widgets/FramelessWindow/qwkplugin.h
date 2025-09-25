9f189832e715e5fcade729f71866c74232d8477b
Layton <31438141+HATTER-LONG@users.noreply.github.com>
2025-09-25 14:19:06 +0800

    avoid warning

---
+++ source/widgets/FramelessWindow/qwkplugin.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <QQmlEngineExtensionPlugin>
+
+class QWKQuickPlugin : public QQmlEngineExtensionPlugin {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID QQmlEngineExtensionInterface_iid)
+};
